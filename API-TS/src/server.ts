3c8dce5e80e739c50ae37dcf897484d05d170a9c
Gustavo <gustavo.hpaula@outlook.com>
2024-08-10 20:20:46 -0300

    feat: testing docker connections

--- API-TS/src/server.ts
+++ API-TS/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json());
 app.use(router);
 
-const port = process.env.PORT ?? 4000
+const port = process.env.NODE_DOCKER_PORT || 8080
 
 app.listen(port, () => console.log('Server rodando na porta ', port))
